5ccb64dcf1a93c5bd6f5aa3f997053bbf3f7a17f
prieyadahal <pdahal@unm.edu>
2025-07-03 01:51:07 -0600

    wrap in navigation

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
 import React from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// Import your screens (we'll create these soon)
 import WelcomeScreen from './Screens/WelcomeScreen';
-import QuizScreen from './Screens/QuizScreen';
 import ResultScreen from './Screens/ResultScreen';
+import QuizScreen from './Screens/QuizScreen';
 
-const Stack = createNativeStackNavigator();
+const stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
+  return(
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Quiz" component={QuizScreen} />
-        <Stack.Screen name="Result" component={ResultScreen} />
-      </Stack.Navigator>
+      <stack.Navigator initialRoute = 'WelcomeScreen'>
+     <stack.Screen name='WelcomeScreen' component={WelcomeScreen} />
+     <stack.Screen name='Quiz' component={QuizScreen}/>
+      </stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
